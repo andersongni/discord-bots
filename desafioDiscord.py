f2277402e3b69c62d0338862e75d35eb81e2cdcb
Anderson Ibrahim <andersongni@users.noreply.github.com>
2023-04-15 17:40:45 -0300

    Version zero

---
+++ desafioDiscord.py
@@ -0,0 +1,55 @@
+import discord
+# from discord import Client
+import re
+
+# Create an Intents object
+intents = discord.Intents.default()
+
+# Specify the intents you want to enable for your bot
+intents.messages = True
+intents.message_content = True
+intents.guilds = True
+
+# print(dir(intents))
+
+# Create a bot instance with the intents
+client = discord.Client(intents=intents)
+
+# Event handler for when the bot is ready
+@client.event
+async def on_ready():
+    print(f'Logged in as {client.user.name}')
+    print('------')
+
+# Event handler for when a message is received
+@client.event
+async def on_message(message):
+    # Check if the message is from the bot itself to avoid infinite loop
+    if message.author == client.user:
+        return
+    
+    print ("-------------------------------------")
+    print ("Received message on channel: " + str(message.channel.id))
+    print ("Author: " + str(message.author))
+    print ("Content: \n" + message.content)
+    print(dir(message))
+
+    # Check if the message starts with a specific command
+    if message.content.startswith('!mplay'):
+        # Get the content of the message
+        content = message.content
+
+        # Define the custom delimiter as "!play " followed by optional newline character
+        delimiter = r"!mplay |\n"
+
+        # Convert message.content to an array of strings, ignoring newline characters after "!play "
+        words = re.split(delimiter, content)
+        
+        # Filter out empty strings from the list
+        for word in words:
+            if word != '':
+                await message.channel.send("!play " + word)
+        
+
+# Run the bot with the bot token
+client.run('MTA5Njg3NjQxMTMxMjE1NjgyNQ.GuPgf8.tmt92rhULm3etfUCPIckzuXO9miJcg5sPFpfNE')
